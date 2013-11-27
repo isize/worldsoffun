655944d7c1fb9c1966ce76f20159c7fba020c446
Stef Wijnberg <s.wijnberg@isize.nl>
2013-11-27 12:38:32 +0100

    Fix

--- web/application/views/failure/send.php
+++
@@ -1,4 +0,0 @@
-<h1>Bedankt voor het melden!</h1>
-
-
-<p>Bedankt voor het melden van de storing. We nemen zo snel mogelijk contact met je op.</p>
--- web/static/js/main.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-	
-	$('.btn-post').click(function(){
-		$(this).closest('form').submit();
-	});
-	
-});
